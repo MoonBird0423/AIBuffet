cd1609d607ecaf473432baf2e02525616f1f4dce
MoonBird0423 <your@email.com>
2025-05-13 23:26:35 +0800

    feat: 更新我的知识库页面

--- frontend/src/pages/MyKnowledge.js
+++ frontend/src/pages/MyKnowledge.js
@@ -70,34 +70,38 @@ function MyKnowledge() {
   }
 
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 mt-16">
-      <div className="flex flex-col lg:flex-row">
-        {/* 左侧知识库列表 */}
-        <div className="w-full lg:w-1/4 lg:pr-6 mb-6 lg:mb-0">
-          <KnowledgeBaseList
-            knowledgeBases={knowledgeBases}
-            selectedKnowledgeBase={selectedKnowledgeBase}
-            onSelect={handleKnowledgeBaseSelect}
-            onListChange={loadKnowledgeBases}
-          />
-        </div>
-        
-        {/* 右侧内容区 */}
-        <div className="w-full lg:w-3/4">
-          {selectedKnowledgeBase ? (
-            <KnowledgeBaseContent
-              knowledgeBase={selectedKnowledgeBase}
-            />
-          ) : (
-            <div className="bg-white rounded-lg shadow-sm p-4 text-center text-gray-500">
-              {knowledgeBases.length === 0 ? 
-                '暂无知识库，点击左侧"+"按钮创建新知识库' : 
-                '请选择一个知识库'
-              }
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      <main className="flex-grow">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+          <div className="flex flex-col lg:flex-row">
+            {/* 左侧知识库列表 */}
+            <div className="w-full lg:w-1/4 lg:pr-6 mb-6 lg:mb-0">
+              <KnowledgeBaseList
+                knowledgeBases={knowledgeBases}
+                selectedKnowledgeBase={selectedKnowledgeBase}
+                onSelect={handleKnowledgeBaseSelect}
+                onListChange={loadKnowledgeBases}
+              />
             </div>
-          )}
+            
+            {/* 右侧内容区 */}
+            <div className="w-full lg:w-3/4">
+              {selectedKnowledgeBase ? (
+                <KnowledgeBaseContent
+                  knowledgeBase={selectedKnowledgeBase}
+                />
+              ) : (
+                <div className="bg-white rounded-lg shadow-sm p-4 text-center text-gray-500">
+                  {knowledgeBases.length === 0 ? 
+                    '暂无知识库，点击左侧"+"按钮创建新知识库' : 
+                    '请选择一个知识库'
+                  }
+                </div>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
+      </main>
     </div>
   );
 }
